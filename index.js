486efd5bbccdc5f4519b8ca2a15e593c15fb5b66
unknown <kylelarsen12@gmail.com>
2022-03-07 20:42:19 -0500

    Started some branches of the story

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if(firstAnswer === 'left'){
     let secondAnswer = window.prompt(`You come across a stray cat. 
 It scampers off down a small hole, just large enough for you to crawl through. 
 Do you follow it, or continue on your path?`)
-    if (secondAnswer === 'follow it') {
+    if (secondAnswer === 'follow it' || secondAnswer === "follow") {
         let secondAnswer = window.prompt('You follow the cat to a colony of cats, nestled in a fort of warm blankets and subsisting off of inexplicably warm soup. They are content with you staying, but you wonder if you should alert the world to this magical safe haven.')
         if(secondAnswer === 'stay') {
             let secondAnswer = window.prompt('You live happily amongs the cats for the rest of your days.')
@@ -13,6 +13,10 @@ Do you follow it, or continue on your path?`)
         }
 
     }
+    else if (secondAnswer === 'continue'){
+        let thirdAnswer = window.prompt("You come across a chamber that reaches upward to a shining light above. There is a long winding staircase, and a much quicker, but rickety-looking ladder that leads up toward the light. Which do you take?")
+    }
+        if
 } else if(firstAnswer === 'right') {
     let secondAnswer = window.prompt(`You come across a snoring dragon. 
 On the other side of him, you see a shiny chest of treasure. Another path would 
